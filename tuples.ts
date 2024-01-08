a87d7f23a3cf78af881d60fdbdc4413483e33778
PeakpoonPK <peakpoonamoroll@gmail.com>
2024-01-08 11:12:58 +0700

    finish for tuplesa

---
+++ tuples.ts
@@ -0,0 +1,17 @@
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40
+};
+
+// type alias
+type Drink = [string, boolean, number];
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clera', true, 40]
+const tea: Drink = ['brown', false, 0];
+
+const carSpaces: [number, number] = [400, 3354]; 
+const carStates = {
+    horsepower: 400,
+    weight: 3354
+}
