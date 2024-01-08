30e9274f33009dbc1685ee88d9306a96110607ca
PeakpoonPK <peakpoonamoroll@gmail.com>
2024-01-08 15:20:00 +0700

    protected

--- classes.ts
+++ classes.ts
@@ -1,21 +1,30 @@
 class Vehicle {
-    public honk(): void {
+    // color: string = 'red';
+    
+    constructor(public color: string){
+        // this.color =color;
+    }
+
+    protected honk(): void {
     console.log('beep')
 }
 }
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color)
 
-class Car extends Vehicle{
-    private drive(): void {
-        console.log('vroom');
-    }
-    startDrivingProcess(): void {
-        this.drive();
-    }
-}
-// const vehicle = new Vehicle();
-// vehicle.drive();
-// vehicle.honk();
+// class Car extends Vehicle{
+//     private drive(): void {
+//         console.log('vroom');
+//     }
+//     startDrivingProcess(): void {
+//         this.drive();
+//         this.honk();
+//     }
+// }
+// // const vehicle = new Vehicle();
+// // vehicle.drive();
+// // vehicle.honk();
 
-const car = new Car();
-car.startDrivingProcess();
-car.honk();
+// const car = new Car();
+// car.startDrivingProcess();
+// car.honk();
